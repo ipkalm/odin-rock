6ef6b9393657fd62cfe3b214ee1f14e7d2d9256c
ipkalm <ipkalm@outlook.com>
2022-09-24 22:16:33 +0700

    add text to html; fix empty user input logic; game till 5 score

--- js/main.js
+++ js/main.js
@@ -1,54 +1,92 @@
 'use strict';
 
 const LIST = ['rock', 'paper', 'scissors'];
-const ROUNDS = 5;
+const WINSCORE = 5;
 
 let getComputerChoice = (list) => {
     const rand = Math.floor(Math.random() * list.length) + 1;
     return list[rand];
 };
 
-let playerSelection = (list) => {
-    const userChoice = prompt(`Choose between ${list}`);
-    if (!userChoice) {
-        console.error(`User must input ${list}`);
-        return;
+let getPlayerSelection = (list) => {
+    let userChoice = '';
+    let errorCounter = 0;
+
+    while (!userChoice) {
+        userChoice = prompt(`Choose between ${list}`);
+
+        if (errorCounter === 3){
+            console.log('User noting input');
+            return false;
+        } else if (!userChoice) {
+            console.log(`User must input ${list}`);
+            errorCounter++;
+        }
     }
 
     return userChoice.toLowerCase();
 };
 
 let playRound = (playerSelection, computerSelection) => {
-
-    if(!playerSelection) {
-        return;
-    }
-
+    let returnValue;
     let msg = 'win';
+
     if ((playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper') ||
         (playerSelection === 'rock' && computerSelection === 'scissors')) {
-        // return player + 1 to score
+        // return true if player win
         msg = `you ${msg}`;
+        returnValue = true;
     } else if ((playerSelection === 'paper' && computerSelection === 'scissors') ||
         (playerSelection === 'scissors' && computerSelection === 'rock') ||
         (playerSelection === 'rock' && computerSelection === 'paper')) {
-        //return computer + 1 to score
+        //return false if computer win
         msg = `computer ${msg}`;
+        returnValue = false;
     } else {
         msg = `friendship ${msg}`;
     }
 
     console.log(msg);
+    return returnValue;
 };
 
 
 let game = () => {
-    for(let i = 0; i < ROUNDS; i++) {
-        console.log(`Round #${i+1}`);
-        playRound(playerSelection(LIST), getComputerChoice(LIST));
+    let playerScore = 0;
+    let computerScore = 0;
+
+    let round = 0;
+    while(true) {
+        if(playerScore === 5) {
+            console.log(`Player win with ${playerScore} score`);
+            break;
+        } else if (computerScore === 5) {
+            console.log(`Computer win with ${computerScore} score`);
+        }
+
+        console.log(`Round #${round + 1}`);
+
+        let playerSelection = getPlayerSelection(LIST);
+        let computerSelection = getComputerChoice(LIST);
+
+        if(!playerSelection) {
+            console.log(`Game exit, because player wont play.`);
+            return false;
+        }
+
+        const roundResult = playRound(playerSelection, computerSelection);
+        if (roundResult === undefined) {
+            console.log(`roundResult is ${roundResult}`);
+        } else if (roundResult === false) {
+            computerScore++;
+        } else if (roundResult === true) {
+            playerScore++;
+        }
+
+        round++;
     }
+
+    console.log(`The game end on ${round} round`);
+    console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`);
 };
-
-
-game();
